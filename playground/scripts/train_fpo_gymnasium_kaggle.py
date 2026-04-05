f1ab8cd2a04a955e52ba563c61c01094addacec7
mp2203 <parikhmanavh@gmail.com>
2026-04-05 07:44:11 -0400

    Increase Kaggle FPO default env count to 16

--- playground/scripts/train_fpo_gymnasium_kaggle.py
+++ playground/scripts/train_fpo_gymnasium_kaggle.py
@@ -75,7 +75,7 @@ class KaggleRunConfig:
     env_name: str = "Ant-v4"
     seed: int = 0
     num_timesteps: int = 1000000
-    num_envs: int = 8
+    num_envs: int = 16
     batch_size: int = 64
     num_minibatches: int = 8
     unroll_length: int = 30
